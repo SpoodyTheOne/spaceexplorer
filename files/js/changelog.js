a05d49a0baad95637e9752ea2120834349918dc6
Spoody <sxsgamer@gmail.com>
2019-08-16 16:05:46 +0200

    fixed lots of stuff

--- files/js/changelog.js
+++ files/js/changelog.js
@@ -6,3 +6,11 @@ changelogs.push(
 <h3>16/8/2019</h3>
 <pre>We added changelogs so you can figure out what we changed and what we didnt change.
 In other news game is still far from ready for release</pre>`);
+
+changelogs.push(
+    `<h2>New commands</h2>
+    <h3>v0.1.0.1</h3>
+    <h3>16/8/2019</h3>
+    <pre>Don't you hate when you get stuck 50 000 000 meters away from everybody else? Fear not, 
+the <strong>!reload</strong> command is here!
+<strong>!reload</strong>,<strong>!rejoin</strong>,<strong>!exit</strong> and <strong>!home</strong> have arrived. Their uses are pretty self explanetory</pre>`);
