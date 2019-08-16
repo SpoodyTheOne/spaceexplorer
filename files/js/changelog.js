c8aa3edf194bbbbb986bba2a4367025ebb747d88
Spoody <sxsgamer@gmail.com>
2019-08-16 13:15:51 +0200

    added changelog

---
+++ files/js/changelog.js
@@ -0,0 +1,8 @@
+var changelogs = new Array();
+
+changelogs.push(
+`<h2>Changelogs are here!</h2>
+<h3>v0.1.0</h3>
+<h3>16/8/2019</h3>
+<pre>We added changelogs so you can figure out what we changed and what we didnt change.
+In other news game is still far from ready for release</pre>`);
